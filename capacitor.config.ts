be1b0b1bd907755fc31a23d1c198b95179d3b85a
Arun <arunrajvv10@gmail.com>
2024-07-12 23:33:24 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,28 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.appseedbed.nearme',
+  appName: 'nearme',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    },
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+    }
+  }
+};
+
+export default config;
